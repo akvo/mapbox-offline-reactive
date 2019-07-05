c5ad307517a0939cdf12d8823f241ef00951b5bd
valeria <valeria@akvo.org>
2019-07-05 13:32:55 +0200

    [#2] fixed code formatting error

--- library/src/main/java/org/akvo/flow/mapbox/offline/reactive/OfflineRegionsLister.kt
+++ library/src/main/java/org/akvo/flow/mapbox/offline/reactive/OfflineRegionsLister.kt
@@ -5,7 +5,7 @@ import com.mapbox.mapboxsdk.offline.OfflineManager
 import com.mapbox.mapboxsdk.offline.OfflineRegion
 import io.reactivex.Single
 
-class OfflineRegionsLister (private val context: Context) {
+class OfflineRegionsLister(private val context: Context) {
 
     fun getRegionsList(): Single<Array<OfflineRegion>> {
         return Single.create { emitter ->
